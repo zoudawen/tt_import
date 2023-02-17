6f71c75580032780810e2f9d727219c4b6d4957a
zou_d <dawenzou1@gmail.com>
2023-02-16 23:00:31 -0500

    tt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='tt_import',
+      version='1.0',
+      description='hello',
+      author='Dawen Zou',
+      author_email='dz@gmail.com',
+      url='https://github.com/zoudawen/tt_import/',
+      packages=['tt_import','tt_import.fun','fun'],
+      py_modules=['tt_import','tt_import.fun','fun']
+     )
